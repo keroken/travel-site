66a18de6fdc8c4359a0378ddcdb5c9698016a3ac
Kenichi Shinagawa <kenichi.shinagawa@gmail.com>
2018-07-22 16:46:57 +0900

    completed large-hero and itro-content

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 cssvars = require('postcss-simple-vars'),
 cssnested = require('postcss-nested'),
 cssimport = require('postcss-import'),
-autoprefixer = require('autoprefixer');
+autoprefixer = require('autoprefixer'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, cssvars, cssnested, autoprefixer]))
+    .pipe(postcss([cssimport, mixins, cssvars, cssnested, autoprefixer]))
     .on('error', function(errorInfo) {
         console.log(errorInfo.toString);
         this.emit('end');
